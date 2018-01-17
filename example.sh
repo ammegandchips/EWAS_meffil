5063d14faabccbeb75db7f2a8f14fc666d28d318
Gemma Sharp <gs8094@bristol.ac.uk>
2018-01-17 16:49:30 +0000

    Removed mention of unnecessary arguments

--- example.sh
+++ example.sh
@@ -6,4 +6,4 @@
 WORK_DIR="/panfs/panasas01/sscm/gs8094/EWAS/example_project"
 module add languages/R-3.0.2
 
-R CMD BATCH --no-save --no-restore '--args Trait CellData CellAdj Phenofile Method Removal BorM TP PACE Covariates Crude_or_Adj WD' /panfs/panasas01/sscm/gs8094/Common_files/meffil_EWAS_script.r /panfs/panasas01/sscm/gs8094/EWAS/example_project/example.out
+R CMD BATCH --no-save --no-restore '--args Trait CellData CellAdj Phenofile BorM TP PACE Covariates WD' /panfs/panasas01/sscm/gs8094/Common_files/meffil_EWAS_script.r /panfs/panasas01/sscm/gs8094/EWAS/example_project/example.out
